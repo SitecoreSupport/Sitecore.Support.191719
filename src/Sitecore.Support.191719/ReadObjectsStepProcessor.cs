9aaf7ed561db4eebbb0d7b090a39b0a5fdb0c1be
DK\pk <pk@sitecore.net>
2018-01-11 13:04:57 +0200

    ReadObjectsStepProcessor is updated to use the fixed AuthenticationClientSettings

---
+++ src/Sitecore.Support.191719/ReadObjectsStepProcessor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Salesforce.Common;
+using Sitecore.DataExchange.Contexts;
+using Sitecore.DataExchange.Models;
+using Sitecore.Support.DataExchange.Providers.Salesforce.Endpoints;
+
+namespace Sitecore.Support.DataExchange.Providers.Salesforce.ReadObjects
+{
+  public class ReadObjectsStepProcessor: Sitecore.DataExchange.Providers.Salesforce.ReadObjects.ReadObjectsStepProcessor
+  {
+    protected override AuthenticationClient GetAuthenticationClient(Endpoint endpoint, PipelineStep pipelineStep,
+      PipelineContext pipelineContext, Sitecore.Services.Core.Diagnostics.ILogger logger)
+    {
+      return endpoint.GetPlugin<AuthenticationClientSettings>().AuthenticationClient;
+    }
+  }
+}
